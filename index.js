0eff6d7311bc0a99a4075a9cff3d4dab3305ba40
Trishten Gregory <gtrishten63@gmail.com>
2021-10-11 16:45:14 -0400

    add api key and call

--- index.js
+++ index.js
@@ -2,8 +2,36 @@ import * as components from "./components";
 import * as state from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import axios from "axios";
+import dotenv from "dotenv";
 
+dotenv.config();
 const router = new Navigo(window.location.origin);
+
+router.hooks({
+  before: (done, params) => {
+    const page =
+      params && params.hasOwnProperty("page")
+        ? capitalize(params.page)
+        : "Home";
+    if (page === "Home") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`
+        )
+        .then(response => {
+          state.Home.weather = {};
+          state.Home.weather.city = response.data.name;
+          state.Home.weather.temp = response.data.main.temp;
+          state.Home.weather.feelsLike = response.data.main.feels_like;
+          state.Home.weather.description = response.data.weather[0].main;
+          done();
+        })
+        .catch(err => console.log(err));
+    }
+  }
+});
+
 router
   .on({
     "/": () => render(state.Home),
