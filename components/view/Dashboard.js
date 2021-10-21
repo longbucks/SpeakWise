404751233729004db3ebddd026434f34b1aeff2a
Trishten Gregory <gtrishten63@gmail.com>
2021-10-21 15:22:57 -0500

    add video input

--- components/view/Dashboard.js
+++ components/view/Dashboard.js
@@ -9,6 +9,12 @@ export default () => html`
       <button>upload a file</button>
       <h2>FeedBack</h2>
       <button>Get feed FeedBack</button>
+      <input type="file" accept="video/*,.mp4" id="input-tag" />
+      <hr />
+      <video controls id="video-tag">
+        <source id="video-source" src="splashVideo" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   </section>
 `;
